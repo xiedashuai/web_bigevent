984fc773509724b257e1c7ae13c49030ecf93948
xiedashuai <1991771727@qq.com>
2020-08-13 11:35:36 +0800

    完成了index

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+$(function () {
+    getUserInfo();
+
+    $("#btnLogout").on("click", function () {
+        layer.confirm("确认退出登录?", {
+            icon: 3,
+            title: "提示"
+        }, function (index) {
+            localStorage.removeItem("token");
+            location.href = "/login.html";
+            layer.close(index);
+        })
+    })
+})
+
+function getUserInfo() {
+    $.ajax({
+        method: "get",
+        url: "/my/userinfo",
+        success(res) {
+            if (res.status != 0) {
+                return layui.layer.msg("获取用户信息失败")
+            }
+            renderAvatar(res.data);
+        }
+    })
+}
+
+function renderAvatar(user) {
+    var name = user.nickname || user.username;
+    $("#welcome").html("欢迎&nbsp;&nbsp;" + name);
+
+    if (user.user_pic != null) {
+        $(".layui-nav-img").attr("src", user.user_pic).show();
+        $(".text-avatar").hide();
+    } else {
+        $(".layui-nav-img").hide();
+        var first = name[0].toUpperCase();
+        $(".text-avatar").html(first).show();
+    }
+}
